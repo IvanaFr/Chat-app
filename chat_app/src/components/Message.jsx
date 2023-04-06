5030aee769cb863c4b73057e277f63198b2948ab
IvanaFr <ivana.frani@gmail.com>
2023-04-06 22:30:21 +0200

    čistim kod

--- chat_app/src/components/Message.jsx
+++ chat_app/src/components/Message.jsx
@@ -2,15 +2,11 @@ import React from 'react';
 
 function Message(props) {
   const { users, content } = props;
-  const messageFromMe = users.id === users.id;
-  const youOrMe = messageFromMe
-    ? "Messages-message users"
-    : "Messages-message";
 
   return (
     <div>
       <ul className="messages-list">
-        <li className={youOrMe}>
+        <li>
           <div className='user'>{users}</div>
           <div className='content'>{content}</div>
         </li> 
